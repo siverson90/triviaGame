b8ad1d60a5c6219b4d628743d89fa0da32688baa
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-09-27 08:11:04 -0700

    adding timer function and new rendering function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,17 @@
 var qAndABank= [
     {
       question: "object 1 question 1",
-      answer: ["tis is answer1","tis is answer2","tis is answer3","tis is answer4"],
+      answer: ["this is answer1","this is answer2","this is answer3","this is answer4"],
       correctAnswer: 1,
     },
     {
       question: "object 2 question 1",
-      answer: ["tis is answer1","tis is answer2","tis is answer3","tis is answer4"],
+      answer: ["this is answer1","this is answer2","this is answer3","this is answer4"],
       correctAnswer: 1,
     },
     {
       question: "object 3 question 1",
-      answer: ["tis is answer1","tis is answer2","tis is answer3","tis is answer4"],
+      answer: ["this is answer1","this is answer2","this is answer3","this is answer4"],
       correctAnswer: 1,
     }
 ]
@@ -20,64 +20,75 @@ var correctAnswers;
 var unanswerQuestions;
 var incorrectAnswers;
 
+var counter= 5;
+var intervalId;
 
 var appendDiv = $("#mainwrapper")
 
 
-
+// ********Render OBJ's to screen*******
 // Create fucntion for looping through elemetns that Calls the HMTL 
 
 function renderQuestions(){
   qAndABank.forEach(function(questionObj) {
-    var questions = renderToScreen(questionObj);
-    $("#questionContainer").append(questions);
-})
+    var questionsForUser = renderToScreen(questionObj);
+    $("#questionContainer").append(questionsForUser);
+  })
 };
 
 
-// Need to have a function for rendering HTML ONLY
+// // Need to have a function for rendering HTML ONLY
 function renderToScreen(questionObj){
-  console.log(questionObj);
-  var newDiv= document.createElement("div");
-  newDiv.className = "question";
-  newDiv.innerText = questionObj.question;
-  for( var i = 0; i < questionObj.answer.length;i++){
-    console.log(questionObj.answer.length);
-  }
-  // inputs maybe need to do a for loop
-  // target questionObj.answer
-
-  return newDiv;
-
-  // add a child for H1
-  // add childinput 1
-
-  // return varible that is the element
+  var questionHeader= document.createElement("h1");
+  var createRadioBtn = document.createElement("INPUT");
+  createRadioBtn.setAttribute("type","radio");
+  console.log(questionObj.question);
+  questionHeader.textContent= questionObj.question;
+  $("#questionContainer").append(questionHeader);
+  questionHeader.addClass = "question";
+  createRadioBtn.addClass= "answer";
+// for( var i = 0; ){}// for each answr add to y.appendChild z
+// in FOR loop if variable i === correctAnswer index in object, then append correct
 }
 
 renderQuestions();
 
-    // createCharHTML: function(player){
-    //     // var characterHTML = "<p class='character'>" + player.characterName + "</p>";
-    //     var characterHTML = document.createElement('p');
-    //     characterHTML.className = "character";
-    //     characterHTML.innerText = player.characterName;
-    //     characterHTML.setAttribute('data-id', player.characterName);
-        
-    //     return characterHTML;
-    // },
+// This window will prompt the user to start the game and when the user click's Start the timer will start to count down.
+
+// *******TIMER*******
+$("#timer").on("click", start)
 
 
-// console.log(questionOrAnswer);
+function start() {
+  intervalId = setInterval(count, 1000);
+}
 
-// function SendToRenderfunction () {
-//   for( var i = 0; i < qAndABank.length; i++)
-//     renderToScreen(qAndABank[i].question,qAndABank[i].answer1, qAndABank[i].answer2, qAndABank[i].answer3,qAndABank[i].answer4,qAndABank[i].correctAnswer);
-// }
+function count(){
+  counter--;
+  console.log(counter);
 
-// SendToRenderfunction();
+  $("#timeDisplay").html("<p>" + counter + "</p>")
+
+  if (counter === 0){
+    stopTimer();
+    alert("game over");
+  }
+
+}
+
+function stopTimer(){
+  clearInterval(intervalId);
+}
+
+console.log(counter);
+
+// ******GAME SCORE LOGIC********
+
+// Game score function- called within timer FUNCTION
+
+// conditional- If radio button is checked && value is correct, else if correct answers ++, if radio button is check incorrect++, else unanswered ++
 
 
 
-// will need to wrap the entire contents
+
 
