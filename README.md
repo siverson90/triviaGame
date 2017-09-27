b76946e7a24010328a5a05c068b374e0da53c018
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-09-26 18:41:07 -0700

    adding new loops and printing

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# triviaGame
-
 Trivia game:
 
 Psuedocode:
@@ -21,7 +19,7 @@ User then sees all the questions on a single form.
 
 ** render function will contain the HTML display character that task's the arugments like Q, A1,A2, A3,A4 and C4 (which are labeled correctly in the array object) add 
 
-When user selects potential answer that question should be logged and counted
+When user selects potential answer via radio button that question should be logged and counted
 ** use a toggle for answered / unanswered OR use a button to assign value
 **
 
@@ -43,6 +41,12 @@ $($div).html (all formatting)
 -- track correct and incorrect and unanswered.
 
 --Have a function called by the timer=== 0 that renders correct As, Incorrect A's and Unanswered
+
+Creattion:
+1. Create Array of Objects
+2. Create start game button and intiailze
+3. create render function- nest For loop; 1 loop creating the div structure and the next function append information into the divs 
+4. 
 
 
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,83 @@
-var questionAndAnswerBank= [
+var qAndABank= [
     {
       question: "object 1 question 1",
-      answer1: "tis is answer1",
-      answer2: "tis is answer2",
-      answer3: "tis is answer3",
-      answer4: "tis is answer4",
-      correctanswer: "this is correct answer"
+      answer: ["tis is answer1","tis is answer2","tis is answer3","tis is answer4"],
+      correctAnswer: 1,
     },
     {
       question: "object 2 question 1",
-      answer1: "tis is answer1",
-      answer2: "tis is answer2",
-      answer3: "tis is answer3",
-      answer4: "tis is answer4",
-      correctanswer: "this is correct answer"
+      answer: ["tis is answer1","tis is answer2","tis is answer3","tis is answer4"],
+      correctAnswer: 1,
     },
     {
       question: "object 3 question 1",
-      answer1: "tis is answer1",
-      answer2: "tis is answer2",
-      answer3: "tis is answer3",
-      answer4: "tis is answer4",
-      correctanswer: "this is correct answer"
+      answer: ["tis is answer1","tis is answer2","tis is answer3","tis is answer4"],
+      correctAnswer: 1,
     }
 ]
 
+var correctAnswers;
+var unanswerQuestions;
+var incorrectAnswers;
+
+
 var appendDiv = $("#mainwrapper")
 
-function renderToScreen(question,a1,a2,a3,a4){
-  var newDiv = $("<div>");
-  newDiv.html("<h1>"+ question + "</h1>" + "<input type=radio>" + a1 + "<input type=radio>"+ a2 + "<input type=radio>"+ a3 + "<input type=radio>"+ a4 + "</button>");
-  appendDiv.append(newDiv);
+
+
+// Create fucntion for looping through elemetns that Calls the HMTL 
+
+function renderQuestions(){
+  qAndABank.forEach(function(questionObj) {
+    var questions = renderToScreen(questionObj);
+    $("#questionContainer").append(questions);
+})
+};
+
+
+// Need to have a function for rendering HTML ONLY
+function renderToScreen(questionObj){
+  console.log(questionObj);
+  var newDiv= document.createElement("div");
+  newDiv.className = "question";
+  newDiv.innerText = questionObj.question;
+  for( var i = 0; i < questionObj.answer.length;i++){
+    console.log(questionObj.answer.length);
+  }
+  // inputs maybe need to do a for loop
+  // target questionObj.answer
+
+  return newDiv;
+
+  // add a child for H1
+  // add childinput 1
+
+  // return varible that is the element
 }
 
-// "<input type='radio'"+  "question=''" "value="''" "checked>
+renderQuestions();
 
-function SendToRenderfunction (){
-  for( var i = 0; i < questionAndAnswerBank.length; i++)
-    renderToScreen(questionAndAnswerBank[i].question,questionAndAnswerBank[i].answer1, questionAndAnswerBank[i].answer2, questionAndAnswerBank[i].answer3,questionAndAnswerBank[i].answer4);
-}
+    // createCharHTML: function(player){
+    //     // var characterHTML = "<p class='character'>" + player.characterName + "</p>";
+    //     var characterHTML = document.createElement('p');
+    //     characterHTML.className = "character";
+    //     characterHTML.innerText = player.characterName;
+    //     characterHTML.setAttribute('data-id', player.characterName);
+        
+    //     return characterHTML;
+    // },
 
-SendToRenderfunction();
+
+// console.log(questionOrAnswer);
+
+// function SendToRenderfunction () {
+//   for( var i = 0; i < qAndABank.length; i++)
+//     renderToScreen(qAndABank[i].question,qAndABank[i].answer1, qAndABank[i].answer2, qAndABank[i].answer3,qAndABank[i].answer4,qAndABank[i].correctAnswer);
+// }
+
+// SendToRenderfunction();
+
+
+
+// will need to wrap the entire contents
 
